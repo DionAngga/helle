1ff11648b8a80d59fb785af8f0a82119c852bb2a
unknown <deonisius97@gmail.com>
2022-06-07 18:47:01 +0700

    repair lint unfinish

--- controller/profile_controller/profile_controller.go
+++ controller/profile_controller/profile_controller.go
@@ -2,6 +2,7 @@ package profilecontroller
 
 import (
 	"encoding/json"
+	"fmt"
 	"helle/entity/request"
 	"helle/entity/response"
 	"helle/usecase"
@@ -21,24 +22,27 @@ func New(usecase usecase.ProfileUseCase) *controller {
 func (c *controller) GetProfilebyUsername(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user *request.Name
-	json.NewDecoder(r.Body).Decode(&user)
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Println("error")
+	}
 	validate := validator.New()
-	errs := validate.Struct(user)
-	if errs != nil {
+	err = validate.Struct(user)
+	if err != nil {
 		w.Header().Add("Content-Type", "application/json")
-		responseBody := errs
-		json.NewEncoder(w).Encode(responseBody)
+		responseBody := err
+		_ = json.NewEncoder(w).Encode(responseBody)
 	} else {
 		User, err := c.usecase.GetProfile(user)
 		if err != nil {
 			responseBody := response.Validate{Validation: "error", Field: "gorm"}
-			json.NewEncoder(w).Encode(responseBody)
+			_ = json.NewEncoder(w).Encode(responseBody)
 		}
 		if User.Username == "" {
 			responseBody := response.Validate{Validation: "error", Field: "username not found"}
-			json.NewEncoder(w).Encode(responseBody)
+			_ = json.NewEncoder(w).Encode(responseBody)
 		} else {
-			json.NewEncoder(w).Encode(&User)
+			_ = json.NewEncoder(w).Encode(&User)
 		}
 	}
 }
